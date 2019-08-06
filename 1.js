7effed0049c590ffe668ffe3b79f3aaad9a4d1e1
Sy Hung <nguyensyhung689@gmail.com>
2019-08-06 17:40:16 +0700

    html css, js : menu slide menu

--- 1.js
+++ 1.js
@@ -1,4 +1,18 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function(){
+    blockdish = $('.alldish').isotope({
+        // options
+        itemSelector: '.onedish',
+        layoutMode: 'masonry'
+    });
+
+    blockdish.imagesLoaded().progress( function() {
+        blockdish.isotope('layout');
+    });
+
+    $('.titledetail a').click(function() {
+        var dulieu = $(this).data('dish');
+        // console.log(dulieu);
+        blockdish.isotope({ filter: dulieu })
+        return false;
+    });
+});
